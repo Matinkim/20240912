d686b7aab09ff4ad40ab04cd06a6aabdfe6b9394
MatinKim <kiki8428@naver.com>
2024-09-19 16:59:06 +0900

    20240919

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
+import { useRef } from "react";
 import Styles from "./Card.module.css";
-
+import postData from "../DUMMY_DATA";
+import Detail from "./Detail";
 export default function Card({
   title,
   content,
@@ -10,29 +12,37 @@ export default function Card({
   userImage,
   comments,
 }) {
+  const modalRef = useRef();
+  function open() {
+    modalRef.current.openModal(); // 모달 열기
+  }
+
   return (
-    <div className={Styles.cardCard}>
-      <div className={Styles.cardImage}>
-        <img src={image} alt="Post image" />
-      </div>
-      <div className={Styles.cardContent}>
-        <h4>{title}</h4>
-        <p>{content}</p>
-        <div className={Styles.cardMeta}>
-          <span className={Styles.createdAt}>{createdAt}</span>
-          <span className={Styles.separator}>.</span>
-          <span className={Styles.comments}>{comments}</span>
+    <>
+      <Detail ref={modalRef} title={title} image={image} content={content} />
+      <div onClick={open} className={Styles.cardCard}>
+        <div className={Styles.cardImage}>
+          <img src={image} alt="Post image" />
+        </div>
+        <div className={Styles.cardContent}>
+          <h4>{title}</h4>
+          <p>{content}</p>
+          <div className={Styles.cardMeta}>
+            <span className={Styles.createdAt}>{createdAt}</span>
+            <span className={Styles.separator}>.</span>
+            <span className={Styles.comments}>{comments}</span>
+          </div>
+        </div>
+        <div className={Styles.cardFooter}>
+          <img
+            src={userImage}
+            alt="Author's avatar"
+            className={Styles.userImage}
+          />
+          <span className={Styles.author}>by {author}</span>
+          <span className={Styles.likes}>{likes}</span>
         </div>
       </div>
-      <div className={Styles.cardFooter}>
-        <img
-          src={userImage}
-          alt="Author's avatar"
-          className={Styles.userImage}
-        />
-        <span className={Styles.author}>by {author}</span>
-        <span className={Styles.likes}>{likes}</span>
-      </div>
-    </div>
+    </>
   );
 }
