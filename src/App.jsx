ce07479565b4618b97a7e0b3c869f3445a4cd0cd
MatinKim <kiki8428@naver.com>
2024-09-20 17:20:25 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
-import postData from "./DUMMY_DATA";
+// import postData from "./DUMMY_DATA";
 import Main from "./components/Main";
 import Form from "./components/Form";
 import Detail from "./components/Detail";
@@ -10,9 +10,24 @@ import Detail from "./components/Detail";
 function App() {
   const [data, setData] = useState("trending");
   const [showCardList, SetShowCardList] = useState(true);
-  const [contentsList, setContentsList] = useState(postData);
+  const [contentsList, setContentsList] =
+    useState("더미데이터 안에 요소가 뭘까");
   const [showModal, setShowModal] = useState(false); // 모달
   const detailRef = useRef();
+
+  useEffect(() => {
+    let data = JSON.parse(localStorage.getItem("key"));
+    //데이터가 존재하지 않는다면
+    if (!data) {
+      //더미데이터를 임시로 넣어주기
+      localStorage.setItem("key", JSON.stringify(dummydata));
+      //로컬스토리지에서 데이터 가져오기
+      data = JSON.parse(localStorage.getItem("key"));
+      // setState({trending:[], feed:[],latest:[]})
+    }
+    //가져온 데이터를 set에 저장
+    setContentsList(data);
+  }, []);
 
   // 교수님 가르쳐주신거
   const list = contentsList[data];
